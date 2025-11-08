de554f09c1db5d0b486d2bbcd9a3224bf9afffb2
Mezgebo Hailu <mezgebohailu@gmail.com>
2025-11-08 14:56:07 +0300

    Fifth commit to the JS-Fundamentals Project

---
+++ 4-concat.js
@@ -0,0 +1,12 @@
+if(process.argv[2] === undefined)
+{
+	console.log(" Undefined is undefined");
+}
+else if(process.argv[3] === undefined)
+{
+	console.log(process.argv[2] + " is undefined");
+}
+else 
+{
+	console.log(process.argv[2] + " is " + process.argv[3]);
+}
